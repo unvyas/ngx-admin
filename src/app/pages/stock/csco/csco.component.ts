6ea786754c7601f8649bbc7e827081a2dd39f86c
richu <richursa@gmail.com>
2020-02-11 10:51:06 +0530

    multiple charts on a page

--- src/app/pages/stock/csco/csco.component.ts
+++ src/app/pages/stock/csco/csco.component.ts
@@ -25,10 +25,15 @@ export class CscoComponent {
   ctx: any;
   canvas2 :any 
   ctx2 :any
+  canvas3 :any
+  ctx3 :any
   ngAfterViewInit() {
     this.canvas = document.getElementById('cs');
     this.ctx = this.canvas.getContext('2d');
-
+    this.canvas2 = document.getElementById('cs2');
+    this.ctx2 = this.canvas2.getContext('2d');
+    this.canvas3 = document.getElementById('cs3');
+    this.ctx3 = this.canvas3.getContext('2d');
     let myChart = new Chart(this.ctx, {
       type: 'line',
       data: {
@@ -50,11 +55,65 @@ export class CscoComponent {
       }
     });
 
+    let myChart2 = new Chart(this.ctx2, {
+      type: 'bar',
+      data: {
+          labels: [],
+          datasets: [{
+              label: 'CSCO Stocks',
+              data: [],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(25, 99, 132, 1)',
+                  'rgba(54, 12, 235, 1)',
+                  'rgba(125, 26,186, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+        responsive: false,
+        display:true
+      }
+    });
+
+    let myChart3 = new Chart(this.ctx3, {
+      type: 'pie',
+      data: {
+          labels: [],
+          datasets: [{
+              label: 'CSCO Stocks',
+              data: [],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(25, 99, 132, 1)',
+                  'rgba(54, 12, 235, 1)',
+                  'rgba(125, 26,186, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+        responsive: false,
+        display:true
+      }
+    });
+
     this.apiService.getApiCall("http://localhost:9200/api/stock/csco")
     .subscribe((data)=>{
       myChart.data.labels = data['labels']
       myChart.data.datasets[0].data = data['data']
       myChart.update()
+      myChart2.data.labels = data['labels']
+      myChart2.data.datasets[0].data = data['data']
+      myChart2.update()
+      myChart3.data.labels = data['labels']
+      myChart3.data.datasets[0].data = data['data']
+      myChart3.update()
     });
 
     
